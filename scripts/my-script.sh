1448dc7cbc7e053182fd6f93d8a4b09cb8fcacf6
Yjunnn <594251995@qq.com>
2024-03-08 09:39:49 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -5,6 +5,6 @@ mkdir -p analysis docs data
 # add a README.md to each directory
 # the scripts directory already exists
 for my_directory in scripts analysis docs data;do
- touch ${my_directory}/README.md
- echo "# ${my_directory}" >> ${my_directory}/README.md
+  touch ${my_directory}/README.md
+  echo "# ${my_directory}" >> ${my_directory}/README.md
 done
